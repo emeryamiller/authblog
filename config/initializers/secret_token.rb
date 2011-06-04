372eb47231bce101f378d7d79620c00514ea0aa8
Ben A. Delat <badelat@gmail.com>
2011-06-04 19:26:34 -0400

    Initial commit of new blogging application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog40::Application.config.secret_token = '5e08802a8b3a6b2c4002c8a8816aed544e070a3a0ae330fc9de9b37b5bc2f48d266daed79069be1c22e593bb871883535b5956aefdbe43b1ad6c4d2f2aebc339'
